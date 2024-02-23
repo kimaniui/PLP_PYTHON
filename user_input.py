f0aace867e85181237b53e7e6a4dcfa7f471f59e
kimaniui <kimanifrank209@gmail.com>
2024-02-23 15:25:33 -0800

    few fixws

--- user_input.py
+++ user_input.py
@@ -1,4 +1,4 @@
 name = input("What is your name?")
 age = input("what is your age?")
-location = ("what is your location?")
+location = input("what is your location?")
 print("Hello" + name + ",you are" + age + "years old and live in" + location + ".")
