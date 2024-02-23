9f191d2e876f795d5a4e6500f415064b335003e4
kimaniui <kimanifrank209@gmail.com>
2024-02-23 14:51:30 -0800

    my first plp ass on github on user input

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+age = input("what is your age?")
+location = ("what is your location?")
+print("Hello" + name + ",you are" + age + "years old and live in" + location + ".")
